f35cd699a6e0c277242e3240bc8435cd299fecc9
YashChaudhary18 <yashchaudhary@t7solution.com>
2026-03-13 16:57:47 +0530

    Add files via upload

---
+++ ThreadSleep/ThreadSleepExample.java
@@ -0,0 +1,30 @@
+package ThreadSleep;
+
+
+
+class SleepThread extends Thread {
+
+    public void run() {
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("Number: " + i);
+
+            try {
+                Thread.sleep(1000); // pause for 1 second
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class ThreadSleepExample {
+
+    public static void main(String[] args) {
+
+        SleepThread t1 = new SleepThread();
+
+        t1.start();
+    }
+}
