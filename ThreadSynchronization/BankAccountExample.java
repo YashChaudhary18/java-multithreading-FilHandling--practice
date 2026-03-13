74bdaa521c0cf7e997a1e8562d8decad9bfdd124
YashChaudhary18 <yashchaudhary@t7solution.com>
2026-03-13 16:55:06 +0530

    Add files via upload

---
+++ ThreadSynchronization/BankAccountExample.java
@@ -0,0 +1,80 @@
+package ThreadSynchronization;
+
+
+
+// BankAccount class
+class BankAccount {
+
+    int balance = 1000;
+
+    // Deposit method
+    public synchronized void deposit(int amount) {
+
+        System.out.println("Depositing: " + amount);
+
+        balance = balance + amount;
+
+        System.out.println("Balance after deposit: " + balance);
+    }
+
+    // Withdraw method
+    public synchronized void withdraw(int amount) {
+
+        if (balance >= amount) {
+
+            System.out.println("Withdrawing: " + amount);
+
+            balance = balance - amount;
+
+            System.out.println("Balance after withdrawal: " + balance);
+
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+}
+
+
+// Deposit Thread
+class DepositThread extends Thread {
+
+    BankAccount account;
+
+    DepositThread(BankAccount account) {
+        this.account = account;
+    }
+
+    public void run() {
+        account.deposit(500);
+    }
+}
+
+
+// Withdraw Thread
+class WithdrawThread extends Thread {
+
+    BankAccount account;
+
+    WithdrawThread(BankAccount account) {
+        this.account = account;
+    }
+
+    public void run() {
+        account.withdraw(700);
+    }
+}
+
+
+public class BankAccountExample {
+
+    public static void main(String[] args) {
+
+        BankAccount account = new BankAccount();
+
+        DepositThread t1 = new DepositThread(account);
+        WithdrawThread t2 = new WithdrawThread(account);
+
+        t1.start();
+        t2.start();
+    }
+}
