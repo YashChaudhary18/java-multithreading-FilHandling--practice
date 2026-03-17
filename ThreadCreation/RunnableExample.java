ec56cfab41a385db2ea5fe0b5f590d26f624a8a1
YashChaudhary18 <yashchaudhary@t7solution.com>
2026-03-17 17:00:18 +0530

    Update RunnableExample.java

--- ThreadCreation/RunnableExample.java
+++ ThreadCreation/RunnableExample.java
@@ -1,16 +1,21 @@
 package ThreadCreation;
 
+// This class implements Runnable to define a task for a thread
 class NumberRunnable implements Runnable {
 
+    // The run() method contains the code that will be executed by the thread
     public void run() {
 
+        // Loop to print numbers from 1 to 10
         for (int i = 1; i <= 10; i++) {
             System.out.println("Runnable Interface: " + i);
 
             try {
+                // Pause the thread for 500 milliseconds (0.5 seconds)
                 Thread.sleep(500);
             } catch (InterruptedException e) {
-                System.out.println(e);
+                // Handle interruption exception
+                System.out.println("Thread interrupted: " + e);
             }
         }
     }
@@ -20,10 +25,13 @@ public class RunnableExample {
 
     public static void main(String[] args) {
 
+        // Create object of Runnable implementation
         NumberRunnable obj = new NumberRunnable();
 
+        // Pass Runnable object to Thread
         Thread t1 = new Thread(obj);
 
-        t1.start(); // start thread
+        // Start the thread (this calls run() internally)
+        t1.start();
     }
 }
